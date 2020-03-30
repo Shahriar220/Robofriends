e5fdc62488f814759f340526a3fcc623ffd34009
Shahriar220 <31791210+Shahriar220@users.noreply.github.com>
2020-03-30 22:53:29 +0600

    This is it

---
+++ build/precache-manifest.b0ea4f4dc42f5c2a1fc0d55fc2165672.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e3bd920019a2bb7fa83a46d2dea54ad",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a3a65d4fcbb730942321",
+    "url": "/static/css/2.3b6f1bda.chunk.css"
+  },
+  {
+    "revision": "86e402091cd2a1a3299c",
+    "url": "/static/css/main.d6f261f7.chunk.css"
+  },
+  {
+    "revision": "a3a65d4fcbb730942321",
+    "url": "/static/js/2.43071c07.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.43071c07.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "86e402091cd2a1a3299c",
+    "url": "/static/js/main.ae06a387.chunk.js"
+  },
+  {
+    "revision": "3a172e9daf29f6cc3071",
+    "url": "/static/js/runtime-main.08f98dcf.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/static/media/SEGA.12a3db8b.woff"
+  }
+]);
